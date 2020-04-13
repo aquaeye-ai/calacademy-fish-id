03c539aff7839547f9a801541627fdc412da2156
jdcast <john.d.cast@gmail.com>
2020-04-13 15:32:33 -0700

    Further experiment with converting tf SavedModel of pretrained quantized model to tflite flatbuffer for later inference

--- lib/tf_saved_model_to_tflite_flatbuffer.py
+++ lib/tf_saved_model_to_tflite_flatbuffer.py
@@ -5,6 +5,11 @@ import tensorflow as tf
 
 EXPORT_DIR = "/home/nightrider/Downloads/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18"
 
+# with tf.Session() as sess:
+#     builder = tf.saved_model.builder.SavedModelBuilder(EXPORT_DIR)
+#     builder.add_meta_graph_and_variables(sess, [tf.saved_model.tag_constants.SERVING],)
+#     builder.save()
+
 # Convert the model.
 converter = tf.lite.TFLiteConverter.from_saved_model(EXPORT_DIR)
 tflite_model = converter.convert()
