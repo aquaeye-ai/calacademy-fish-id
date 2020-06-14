2dfa0373a645005965380e0eb475f7ab8e6554b6
jdcast <john.d.cast@gmail.com>
2020-06-14 13:55:16 -0700

    Image Scraping -> Add object detection bounding box scraper

--- lib/image_scraping/obj_det_dataset_scraper.py
+++ lib/image_scraping/obj_det_dataset_scraper.py
@@ -2,31 +2,65 @@
 
 import os
 import xml
+import cv2
 import yaml
 
 import lib.file_utils as fu
 
 if __name__ == "__main__":
     # we expect, as a hand-shake agreement, that there is a .yml config file in top level of lib/configs directory
-    config_dir = os.path.join(os.curdir, 'configs')
-    yaml_path = os.path.join(config_dir, 'scrape_obj_det_dataset.yml')
+    config_dir = os.path.join(os.pardir, 'configs', 'image_scraping')
+    yaml_path = os.path.join(config_dir, 'obj_det_dataset_scraper.yml')
     with open(yaml_path, "r") as stream:
         config = yaml.load(stream)
 
     ## collect hyper parameters/args from config
     # NOTE: float() is required to parse any exponentials since YAML sends exponentials as strings
-    directory = config["directory"]
+    src_ann_directory = config["src_ann_directory"]
+    src_img_directory = config["src_img_directory"]
+    dst_img_directory = config["dst_img_directory"]
     annotation_extension = config["annotation_extension"]
 
     classes = []
-    ann_paths = fu.find_files(directory=directory, extension=annotation_extension)
+    ann_paths = fu.find_files(directory=src_ann_directory, extension=annotation_extension)
 
     for idx, ann_path in enumerate(ann_paths):
         et = xml.etree.ElementTree.parse(ann_path)
         root = et.getroot()
 
+        img_filename = None
+        img_path = None
+        img = None
+        idx = 0
         for elem in root.getiterator():
-            if elem.tag == 'object':
+            if elem.tag == 'filename':
+                # open image
+                img_filename = elem.text
+                img_path = os.path.join(src_img_directory, img_filename)
+                img = cv2.imread(img_path)
+            elif elem.tag == 'object':
+                label =  elem._children[0].text
+                if label not in classes:
+                    classes.append(label)
+                    # print("Found '{}' for ann_path: {}".format(label, ann_path))
 
-                classes.append(elem._children[0].text)
-                print("Found '{}' for ann_path: {}".format(elem._children[0].text, ann_path))
+                # collect bounding box coordinates
+                xmin = int(elem._children[4]._children[0].text)
+                ymin = int(elem._children[4]._children[1].text)
+                xmax = int(elem._children[4]._children[2].text)
+                ymax = int(elem._children[4]._children[3].text)
+
+                # gather image crop
+                crop = img[ymin:ymax+1, xmin:xmax+1]
+                h, w = crop.shape[0:2]
+                # print('Shape: {}'.format(crop.shape[0:2]))
+
+                if h > 0 and w > 0:
+                    fu.init_directory(directory=os.path.join(dst_img_directory, label))
+                    cv2.imwrite(os.path.join(dst_img_directory, label, img_filename[:-4]+'_crop_{}.jpg'.format(idx)), crop)
+                else:
+                    print("Invalid crop: {}; xmin={}, ymin={}, xmax={}, ymax={}".format(img_filename, xmin, ymin, xmax, ymax))
+
+                idx+=1
+
+    print("Classes: {}".format(classes))
