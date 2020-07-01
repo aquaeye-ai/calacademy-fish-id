7afd7193c8c4109cf7826441d720343da68f2b14
jdcast <john.d.cast@gmail.com>
2020-06-30 18:19:25 -0700

    Add script to convert all png images to jpg format in directory as well as remove those png images after conversion

---
+++ lib/png_to_jpg_for_directory.py
@@ -0,0 +1,38 @@
+"""
+Script that wraps mogrify and rm to convert all png images to jpg format and then remove the png images if desired.
+"""
+import os
+import yaml
+import subprocess
+
+# from tensorflow.python.tools import freeze_graph
+
+if __name__ == "__main__":
+    # we expect, as a hand-shake agreement, that there is a .yml config file in top level of lib/configs directory
+    config_dir = os.path.join(os.curdir, 'configs')
+    yaml_path = os.path.join(config_dir, 'png_to_jpg_for_directory.yml')
+    with open(yaml_path, "r") as stream:
+        config = yaml.load(stream)
+
+    ## collect hyper parameters/args from config
+    # NOTE: float() is required to parse any exponentials since YAML sends exponentials as strings
+    source_directory = config["source_directory"]
+    remove_png = config["remove_png"]
+
+    # convert all png to jpg
+    mog_process = subprocess.Popen("mogrify -verbose -format jpg {}/*.png".format(source_directory),
+                                   shell=True)
+                                    # stdout=subprocess.PIPE,
+                                    # stderr=subprocess.PIPE)
+
+    # Popen is asynchronous and mogrify takes a while for large directorys, so we must wait for it to complete before
+    # kicking off the command to start removing images.  Otherwise, rm will run and remove images before mogrify has
+    # converted them.
+    mog_process.wait()
+
+    # remove all png if desired
+    if remove_png > 0:
+        subprocess.Popen("rm -v {}/*.png".format(source_directory),
+                         shell=True)
+                         # stdout=subprocess.PIPE,
+                         # stderr=subprocess.PIPE)
