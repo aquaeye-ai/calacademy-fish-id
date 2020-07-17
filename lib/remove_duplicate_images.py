a83125a50e4da4268de1775e382110fbc2f8ef3c
jdcast <john.d.cast@gmail.com>
2020-07-16 23:25:06 -0700

    Add sorting and more manual inspection options for remove_duplicate_images.py

--- lib/remove_duplicate_images.py
+++ lib/remove_duplicate_images.py
@@ -1,17 +1,18 @@
 import os
 import cv2
-import argparse
+import shutil
 
 import numpy as np
 import file_utils as fu
-from imutils import paths
+
+BASE_DIRECTORY = "/home/nightrider/calacademy-fish-id/datasets/image_classification/pcr/all_classes/acanthurus_lineatus"
+SOURCE_DIRECTORY = os.path.join(BASE_DIRECTORY, "combined")
+POTENTIAL_DUPLICATES_DIRECTORY = os.path.join(BASE_DIRECTORY, "potential_duplicates")
+AUTO_REMOVE = 0
 
 
-DIRECTORY = "/home/nightrider/calacademy-fish-id/datasets/image_classification/pcr/all_classes/acanthurus_triostegus/combined"
-REMOVE = 0
 
-
-def dhash(image, hashSize=8):
+def dhash(image, hashSize=4):#8):
 	# convert the image to grayscale and resize the grayscale image,
 	# adding a single column (width) so we can compute the horizontal
 	# gradient
@@ -28,7 +29,7 @@ if __name__ == "__main__":
 	# grab the paths to all images in our input dataset directory and then initialize our hashes dictionary
 
 	print("[INFO] computing image hashes...")
-	imagePaths = fu.find_files(directory=DIRECTORY, extension=".jpg")
+	imagePaths = fu.find_files(directory=SOURCE_DIRECTORY, extension=".jpg")
 	hashes = {}
 
 	# loop over our image paths
@@ -39,7 +40,21 @@ if __name__ == "__main__":
 		# grab all image paths with that hash, add the current image
 		# path to it, and store the list back in the hashes dictionary
 		p = hashes.get(h, [])
-		p.append(imagePath)
+
+		# sort the image paths by image size -> largest to smallest
+		# this makes it easy to retain the high resolution images when removing duplicates later
+		new_img_size = image.shape[0]*image.shape[1]
+		insert_idx = len(p)
+		for idx, img_path in enumerate(p):
+			img = cv2.imread(img_path)
+			img_size = img.shape[0]*img.shape[1]
+
+			if new_img_size > img_size:
+				insert_idx = idx
+				break
+
+		# p.append(imagePath)
+		p.insert(insert_idx, imagePath)
 		hashes[h] = p
 
 	# count the duplicate sets for visibility into data
@@ -47,14 +62,14 @@ if __name__ == "__main__":
 	for (h, hashedPaths) in hashes.items():
 		if len(hashedPaths) > 1:
 			num_duplicate_sets += 1
-	print("Found {} duplicate sets".format(num_duplicate_sets))
+	print("[INFO] Found {} duplicate sets".format(num_duplicate_sets))
 
 	# loop over the image hashes
 	for (h, hashedPaths) in hashes.items():
 		# check to see if there is more than one image with the same hash
 		if len(hashedPaths) > 1:
 			# check to see if this is a dry run
-			if REMOVE <= 0:
+			if AUTO_REMOVE <= 0:
 				# initialize a montage to store all images with the same
 				# hash
 				montage = None
@@ -63,7 +78,8 @@ if __name__ == "__main__":
 					# load the input image and resize it to a fixed width
 					# and heightG
 					image = cv2.imread(p)
-					image = cv2.resize(image, (150, 150))
+					print("[INFO] path: {}; size: {}".format(p, image.shape[0:2]))
+					image = cv2.resize(image, (512, 512))
 					# if our montage is None, initialize it
 					if montage is None:
 						montage = image
@@ -72,8 +88,31 @@ if __name__ == "__main__":
 						montage = np.hstack([montage, image])
 				# show the montage for the hash
 				print("[INFO] hash: {}".format(h))
-				cv2.imshow("Montage", montage)
-				cv2.waitKey(0)
+				cv2.imshow("Press 'd' to remove duplicates; Any other key to continue", montage)
+
+				# wait for user defined action before proceeding
+				k = cv2.waitKey(0)
+				if k == ord('d'): # remove duplicates
+					# loop over all image paths with the same hash *except*
+					# for the first image in the list (since we want to keep
+					# one, and only one, of the duplicate images)
+					for p in hashedPaths[1:]:
+						print("Removing: {}".format(p))
+						os.remove(p)
+				elif k == ord('m'): # move to a folder for further analysis
+					# loop over all image paths with the same hash *except*
+					# for the first image in the list (since we want to keep
+					# one, and only one, of the duplicate images)
+					for p in hashedPaths[1:]:
+						print("Moving: {}".format(p))
+
+						# initialize potential duplicates directory if it doesn't exist
+						fu.init_directory(directory=POTENTIAL_DUPLICATES_DIRECTORY)
+
+						# move file
+						basename = os.path.basename(p)
+						shutil.move(p, os.path.join(POTENTIAL_DUPLICATES_DIRECTORY, basename))
+
 			# otherwise, we'll be removing the duplicate images
 			else:
 				# loop over all image paths with the same hash *except*
