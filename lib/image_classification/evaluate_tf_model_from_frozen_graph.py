47d9d0c2430574df94742261ce64cf6b12ba70d9
jdcast <john.d.cast@gmail.com>
2020-07-30 15:23:59 -0700

    Image Classification -> Finish test set evaluation script for tf models from frozen graphs

--- lib/image_classification/evaluate_tf_model_from_frozen_graph.py
+++ lib/image_classification/evaluate_tf_model_from_frozen_graph.py
@@ -1,3 +1,22 @@
+"""
+Script to evaluate and provide Top-1 and Top-K stats on test data for a TF model from frozen graph.
+For use with pretrained TF Slim models (e.g. those used with lib/image_classification/inference_pretrained_tf_model_from_frozen_graph.py)
+and those models fine-tuned/trained using  lib/image_classification/retrain_1_b_1.py.
+
+NOTE: the log.txt will be output in a parent directory as compared to the output_dir provided to the script.
+
+Stats produced by script:
+1) Top-1 Accuracy per class
+2) Top-K Accuracy per class
+3) Top-K most common incorrect labels per class with frequency occurrence counts
+4) Total number of images evaluated per class
+5) Average Top-1 Accuracy across all classes
+6) Average Top-K Accuracy across all classes
+7) List of images organized by class whose Top-1 prediction was incorrect, along with their Top-K most common labels/probabilities
+8) List of images organized by class whose Top-K predictions were all incorrect, along with their Top-K most common labels/probabilities
+"""
+
+
 import os
 import yaml
 import time
@@ -65,6 +84,7 @@ def def_preprocess_ops(image, height, width, central_fraction=0.875, scope=None,
     """
 
     image_data = tf.image.convert_image_dtype(image, dtype=tf.float32)
+
     # Crop the central region of the image with an area containing 87.5% of
     # the original image.
     if central_fraction and apply_tf_slim_preprocessing == True:
@@ -73,8 +93,7 @@ def def_preprocess_ops(image, height, width, central_fraction=0.875, scope=None,
     if height and width:
         # Resize the image to the specified height and width.
         image_data = tf.expand_dims(image_data, 0)
-        image_data = tf.image.resize_bilinear(image_data, [height, width],
-                                         align_corners=False)
+        image_data = tf.image.resize_bilinear(image_data, [height, width], align_corners=False)
         image_data = tf.squeeze(image_data, [0])
 
     if apply_tf_slim_preprocessing == True:
@@ -83,14 +102,17 @@ def def_preprocess_ops(image, height, width, central_fraction=0.875, scope=None,
 
     return image_data
 
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
-
 @log_utils.timeit
-def run_inference_for_multiple_images(images=None, graph=None, sess=None, output_node=None, input_node=None, input_tensor=None,
-                                      tensor_dict=None):
+def run_inference_for_multiple_images(images=None, output_node=None, input_tensor=None, tensor_dict=None):
+    """
+    Call sess.run on full graph for inference of batch of images (can be single image).
+
+    :param images: numpy array of images
+    :param output_node: str, name of output node in graph
+    :param input_tensor: str, name of input node in graph
+    :param tensor_dict: dict of tf tensors and arguments to supply session.run() call with
+    :return: dict of outputs from inference
+    """
     # we need to expand the first dimension if we only inference on one image, since the model inference expects a batch
     if len(images.shape) == 3:
         images = np.expand_dims(images, 0)
@@ -106,18 +128,24 @@ def run_inference_for_multiple_images(images=None, graph=None, sess=None, output
 
     return output_dict
 
-def predict_images_whole(test_image_paths=None, category_index=None, min_score_threshold=None, model_input_size=None,
-                         output_node=None, input_node=None, apply_tf_slim_preprocessing=False, K=None, preprocess_op=None,
-                         image_placeholder=None, input_tensor=None, tensor_dict=None, load_img_op=None,
-                         image_path_placeholder=None):
+def predict_images_whole(test_image_paths=None, category_index=None, output_node=None, input_node=None, K=None,
+                         preprocess_op=None, image_placeholder=None, input_tensor=None, tensor_dict=None,
+                         load_img_op=None, image_path_placeholder=None):
     """
     Inferences model on entire image for each image path supplied.
 
     :param test_image_paths: list of strings, paths to images
     :param category_index: dictionary, key=class index in output and value=class name (string)
-    :param min_score_threshold: float, minimum prediction confidence to accept in the output
-    :param model_input_size: int, taken as nxn dimensions of model input
-    :return: None
+    :param output_node: str, name of output node in graph
+    :param input_node: str, name of input node in graph
+    :param K: int, K value for Top-K stats
+    :param preprocess_op: tf op to apply for preprocessing
+    :param image_placeholder: tf placeholder for image
+    :param input_tensor: tf tensor for graph input
+    :param tensor_dict: tensor dict arg for sess.run of full graph
+    :param load_img_op: tf op to apply to load image
+    :param image_path_placeholder: tf placeholder to hold path to image
+    :return: list of tuples where each tuple = (image path, class name, sorted-descending list of Top-K class predictions)
     """
     results = []
     for im_idx, image_path in enumerate(test_image_paths):
@@ -135,9 +163,8 @@ def predict_images_whole(test_image_paths=None, category_index=None, min_score_t
         ## Actual detection.
         # Both of these produce the same but I use Reshape_1 to stay in line with tf slim's tutorial: https://github.com/tensorflow/models/tree/master/research/slim#Export
         # output_node = 'InceptionV3/Predictions/Softmax'
-        output_dict = run_inference_for_multiple_images(image_np, detection_graph, sess=sess, output_node=output_node,
-                                                        input_node=input_node, input_tensor=input_tensor,
-                                                        tensor_dict=tensor_dict)
+        output_dict = run_inference_for_multiple_images(images=image_np, output_node=output_node,
+                                                        input_tensor=input_tensor, tensor_dict=tensor_dict)
 
         class_scores = output_dict[output_node][0]
 
@@ -155,7 +182,7 @@ def predict_images_whole(test_image_paths=None, category_index=None, min_score_t
 if __name__ == "__main__":
     # we expect, as a hand-shake agreement, that there is a .yml config file in top level of lib/configs directory
     config_dir = os.path.join(os.curdir, '..', 'configs', 'image_classification')
-    yaml_path = os.path.join(config_dir, 'evaluate_model.yml')
+    yaml_path = os.path.join(config_dir, 'evaluate_tf_model_from_frozen_graph.yml')
     with open(yaml_path, "r") as stream:
         config = yaml.load(stream)
 
@@ -178,7 +205,6 @@ if __name__ == "__main__":
     # initialize output directory
     fu.init_directory(directory=output_dir)
 
-    # grab image paths
     # get class directories
     class_image_paths = {}
     class_dirs = [d for d in os.listdir(path_to_test_images_dir)]
@@ -194,7 +220,8 @@ if __name__ == "__main__":
         with open(path_to_labels) as labels_f:
             for idx, line in enumerate(labels_f):
                 category_index[idx] = line.strip()
-    
+
+    # begin instantiating graph and ops
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         with tf.Session() as sess:
@@ -235,6 +262,8 @@ if __name__ == "__main__":
 
                             t1 = time.time()
                             total_num_images = 0
+                            total_accuracy_top_1 = 0.0
+                            total_accuracy_top_k = 0.0
                             for cls_idx, class_dir in enumerate(class_dirs):
                                 logger.info("{}/{} Class: {}".format(cls_idx, len(class_dirs), class_dir))
                                 class_image_paths = fu.find_images(directory=os.path.join(path_to_test_images_dir, class_dir),
@@ -243,11 +272,8 @@ if __name__ == "__main__":
 
                                 results = predict_images_whole(test_image_paths=class_image_paths,
                                                                category_index=category_index,
-                                                               min_score_threshold=min_score_threshold,
-                                                               model_input_size=model_input_size,
                                                                output_node=output_node,
                                                                input_node=input_node,
-                                                               apply_tf_slim_preprocessing=apply_tf_slim_preprocessing,
                                                                K=K,
                                                                preprocess_op=preprocess_op,
                                                                image_placeholder=image_placeholder,
@@ -259,23 +285,24 @@ if __name__ == "__main__":
                                 # gather Top-1 stats
                                 num_incorrect_top_1 = 0
                                 image_paths_incorrect_top_1 = []
-                                incorrect_class_votes = {}
+                                incorrect_class_votes_dict = {}
                                 for result in results:
                                     if result[1][0] != class_dir:
-                                        if result[1][0] in incorrect_class_votes:
-                                            incorrect_class_votes[result[1][0]] += 1
+                                        if result[1][0] in incorrect_class_votes_dict:
+                                            incorrect_class_votes_dict[result[1][0]] += 1
                                         else:
-                                            incorrect_class_votes[result[1][0]] = 0
+                                            incorrect_class_votes_dict[result[1][0]] = 1
                                         num_incorrect_top_1 += 1
-                                        image_paths_incorrect_top_1.append((result[0], result[1][0]))
+
+                                        # provide the tuples of (image path, Top-K most common incorrect labels, probabilities of top-K most common incorrect labels)
+                                        image_paths_incorrect_top_1.append((result[0], result[1][:K], result[2][:K]))
 
                                 percent_incorrect_top_1 = (np.float(num_incorrect_top_1) / np.float(len(results))) * 100.0
+                                total_accuracy_top_1 += (100.0 - percent_incorrect_top_1)
 
-                                incorrect_label_top_1 = None
                                 max_num_votes = 0
-                                for label, num_votes in incorrect_class_votes.items():
-                                    if num_votes > max_num_votes and label != class_dir:
-                                        incorrect_label_top_1 = label
+                                incorrect_class_votes_list = [(key, value) for key, value in incorrect_class_votes_dict.items()]
+                                incorrect_class_votes_list_sorted = sorted(incorrect_class_votes_list, key=lambda tup: tup[1], reverse=True)
 
                                 # gather Top-K stats
                                 num_incorrect_top_k = 0
@@ -283,29 +310,59 @@ if __name__ == "__main__":
                                 for result in results:
                                     if class_dir not in result[1]:
                                         num_incorrect_top_k += 1
-                                        image_paths_incorrect_top_k.append(result[0])
+
+                                        # provide the tuples of (image path, Top-K most common incorrect labels, probabilities of top-K most common incorrect labels)
+                                        image_paths_incorrect_top_k.append((result[0], result[1][:K], result[2][:K]))
 
                                 percent_incorrect_top_k = (np.float(num_incorrect_top_k) / np.float(len(results))) * 100.0
+                                total_accuracy_top_k += (100.0 - percent_incorrect_top_k)
 
-                                # write stats to file
-                                stats_f.write("{}:\n \tTop-1 Accuracy = {}%, Top-{} Accuracy: {}, "
-                                              "Top-1 Incorrect Label: {}, "
+                                ## write stats to file
+
+                                # gather Top-K most common, incorrect labels for class
+                                incorrect_labels_and_probs_class_top_k = None
+                                if len(incorrect_class_votes_list_sorted) >= K:  # we may have fewer than K incorrect labels if this class performed well
+                                    incorrect_labels_and_probs_class_top_k = str([': '.join(map(str, tup)) for tup in incorrect_class_votes_list_sorted[:K]])
+                                else:
+                                    incorrect_labels_and_probs_class_top_k = str([': '.join(map(str, tup)) for tup in incorrect_class_votes_list_sorted[:]])
+
+                                # write general stats
+                                stats_f.write("{}:\n \tTop-1 Accuracy = {:.2f}%, Top-{} Accuracy: {:.2f}%, "
+                                              "Top-{} Incorrect Labels: {}, "
                                               "Total Images: {}\n".format(class_dir,
                                                                           100.0 - percent_incorrect_top_1, K,
-                                                                          100.0 - percent_incorrect_top_k,
-                                                                          incorrect_label_top_1,
+                                                                          100.0 - percent_incorrect_top_k, K,
+                                                                          incorrect_labels_and_probs_class_top_k,
                                                                           len(class_image_paths)))
                                 stats_f.flush()
 
+                                # write predictions stats for images whose true class didn't match their Top-1 prediction
                                 misclassifications_top_1_f.write("{}:\n".format(class_dir))
                                 for mis in image_paths_incorrect_top_1:
-                                    misclassifications_top_1_f.write("\t{}\n".format(mis))
+                                    # write filename
+                                    misclassifications_top_1_f.write("\t{}\n".format(os.path.basename(mis[0])))
+
+                                    # write top-K most common labels/probabilities
+                                    misclassifications_top_1_f.write("\t\t{}\n".format(
+                                        [': '.join(map(str, tup)) for tup in zip(mis[1][:], mis[2][:])]))
                                     misclassifications_top_1_f.flush()
 
+                                # write predictions stats for images whose true class didn't match their Top-K predictions
                                 misclassifications_top_k_f.write("{}:\n".format(class_dir))
                                 for mis in image_paths_incorrect_top_k:
-                                    misclassifications_top_k_f.write("\t{}\n".format(mis))
+                                    # write filename
+                                    misclassifications_top_k_f.write("\t{}\n".format(os.path.basename(mis[0])))
+
+                                    # write top-K most common labels/probabilities
+                                    misclassifications_top_k_f.write("\t\t{}\n".format(
+                                        [': '.join(map(str, tup)) for tup in zip(mis[1][:], mis[2][:])]))
                                     misclassifications_top_k_f.flush()
+
+                            # write average Top-1 and Top-K accuracies across classes
+                            stats_f.write("\nAverage Top-1 Accuracy: {:.2f}%\n".format(
+                                total_accuracy_top_1 / np.float(len(class_dirs))))
+                            stats_f.write("Average Top-{} Accuracy: {:.2f}%".format(
+                                K, total_accuracy_top_k / np.float(len(class_dirs))))
 
                             t2 = time.time()
                             logger.info("Total Images: {}".format(total_num_images))
