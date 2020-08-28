ef5b8527f0b1f977f7cd5acf27875ef591a09d3c
jdcast <john.d.cast@gmail.com>
2020-08-28 16:15:22 -0700

    Restructure lib

--- lib/scripts/object_detection/inference_pretrained_tf_obj_detect_model.py
+++ lib/scripts/object_detection/inference_pretrained_tf_obj_detect_model.py
@@ -448,7 +448,7 @@ def predict_images_whole(test_image_paths=None, category_index=None, min_score_t
 
 if __name__ == "__main__":
     # we expect, as a hand-shake agreement, that there is a .yml config file in top level of lib/configs directory
-    config_dir = os.path.join(os.curdir, 'configs')
+    config_dir = os.path.join(os.curdir, 'configs', 'object_detection')
     yaml_path = os.path.join(config_dir, 'inference_pretrained_tf_obj_detect_model.yml')
     with open(yaml_path, "r") as stream:
         config = yaml.load(stream)
@@ -472,7 +472,7 @@ if __name__ == "__main__":
     # Path to frozen detection graph. This is the actual model that is used for the object detection.
     model_file = model_name + '.tar.gz'
     # path_to_frozen_graph = os.path.join(model_name, 'frozen_inference_graph.pb')
-    path_to_frozen_graph = '/home/nightrider/calacademy-fish-id/classifiers/models/ssd_mobilenet_v2_coco_2018_03_29/fine_tuned/5_15_2020/frozen_inference_graph.pb'
+    path_to_frozen_graph = '/home/nightrider/calacademy-fish-id/classifiers/models/ssd_mobilenet_v2_coco_2018_03_29/fine_tuned/5_23_2020/frozen_inference_graph.pb'
 
     # List of the strings that is used to add correct label for each box.
     # path_to_labels = os.path.join('/home/nightrider/tensorflow/models/research/object_detection', 'data', label_map)
