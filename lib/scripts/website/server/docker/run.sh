4209ae135efd1283a6caedd42e2574c9193b43f1
jdcast <john.d.cast@gmail.com>
2020-09-25 14:59:17 -0700

    Website -> Fix bug in model path

--- lib/scripts/website/server/docker/run.sh
+++ lib/scripts/website/server/docker/run.sh
@@ -5,7 +5,7 @@ source config.sh
 read -p "Enter the full path to the github directory.  This will be exposed at /share.  Example: '~/aquaeye_ai':" PROJ_DIR
 PROJ_DIR=$(eval echo $PROJ_DIR)
 
-read -p "Enter the full path to the model files directory.  This will be exposed at /share/model.  Example: '~/aquaeye_ai/models':" PROJ_DIR
+read -p "Enter the full path to the model files directory.  This will be exposed at /share/model.  Example: '~/aquaeye_ai/models':" MODEL_DIR
 MODEL_DIR=$(eval echo $MODEL_DIR)
 
 # Expose ports and run
