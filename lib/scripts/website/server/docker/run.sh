d7543e51ba772c4a4655a0fdde3b6471914dbffa
jdcast <john.d.cast@gmail.com>
2020-09-25 12:30:36 -0700

    Website -> Make model dir a variable

--- lib/scripts/website/server/docker/run.sh
+++ lib/scripts/website/server/docker/run.sh
@@ -2,12 +2,15 @@
 
 source config.sh
 
-read -p "Enter the full path to the github directory.  This will be exposed at /share.  Example: '~/guardian_demo':" PROJ_DIR
+read -p "Enter the full path to the github directory.  This will be exposed at /share.  Example: '~/aquaeye_ai':" PROJ_DIR
 PROJ_DIR=$(eval echo $PROJ_DIR)
+
+read -p "Enter the full path to the model files directory.  This will be exposed at /share/model.  Example: '~/aquaeye_ai/models':" PROJ_DIR
+MODEL_DIR=$(eval echo $MODEL_DIR)
 
 # Expose ports and run
 sudo docker run -it \
         -p $PUBLIC_PORT:$PUBLIC_PORT \
         -v "$PROJ_DIR":/share \
-        -v /home/nightrider/calacademy-fish-id/classifiers/image_classification/models/mobilenet_v2_100_224_keras/fine_tuned/9_10_2020/1:/share/model \
+        -v "$MODEL_DIR":/share/model \
         --name $IMAGE_NAME $IMAGE_NAME
