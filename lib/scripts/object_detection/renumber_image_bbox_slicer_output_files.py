d3f5517b39b38c8ed031abbfa860e5d3ee4972be
jdcast <john.d.cast@gmail.com>
2021-03-30 17:24:36 -0700

    Renumber bbox files for new Reef Lagoon od data

--- lib/scripts/object_detection/renumber_image_bbox_slicer_output_files.py
+++ lib/scripts/object_detection/renumber_image_bbox_slicer_output_files.py
@@ -3,12 +3,12 @@
 import os
 import yaml
 
-import file_utils as fu
+import lib.scripts.file_utils as fu
 
 if __name__ == "__main__":
     # we expect, as a hand-shake agreement, that there is a .yml config file in top level of lib/configs directory
-    config_dir = os.path.join(os.curdir, 'configs')
-    yaml_path = os.path.join(config_dir, 'renumber_image_bbox_slicer_output_files.yml')
+    config_dir = os.path.join(os.curdir, '..', '..', 'configs')
+    yaml_path = os.path.join(config_dir, 'object_detection', 'renumber_image_bbox_slicer_output_files.yml')
     with open(yaml_path, "r") as stream:
         config = yaml.load(stream)
 
